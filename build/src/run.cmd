159c366e5c2ec6f0f5625d013615170817322f31
mkuba50 <mkuba50@outlook.com>
2018-12-06 20:22:14 +0100

    Fix a few bugs in cmd script of archive version reported by abbodi1406

--- build/src/run.cmd
+++ build/src/run.cmd
@@ -1,9 +1,9 @@
 @echo off
-REG QUERY HKU\S-1-5-19\Environment >NUL 2>&1 
-IF %ERRORLEVEL% EQU 0 goto :HAS_ADMIN
-
 if exist "%~dp0\files\src\UUPDUMP_translation.ini" del "%~dp0\files\src\UUPDUMP_translation.ini"
 if exist "%~dp0UUPDUMP_translation.ini" copy "%~dp0UUPDUMP_translation.ini" "%~dp0\files\src\UUPDUMP_translation.ini"
+
+REG QUERY HKU\S-1-5-19\Environment >NUL 2>&1 
+IF %ERRORLEVEL% EQU 0 goto :HAS_ADMIN
 
 set "command="""%~dp0files\src\uupdownloader.ahk""" """%~dp0""""
 set "command=%command:'=''%"
@@ -15,5 +15,5 @@ powershell Start-Process -FilePath '%filePath%' -ArgumentList '%command%' -Verb 
 exit /b
 
 :HAS_ADMIN
-start "%~dp0files\AutoHotkey.exe" "%~dp0files\src\uupdownloader.ahk" "%~dp0"
+start "" "%~dp0files\AutoHotkey.exe" "%~dp0files\src\uupdownloader.ahk" "%~dp0"
 exit /b
